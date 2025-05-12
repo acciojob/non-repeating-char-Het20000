915e67427806c9d11f76f5debddd9a160a672b45
Het20000 <hp985383@gmail.com>
2025-05-12 10:29:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(s) {
+  const charCounts = {};
+  for (const char of s) {
+    charCounts[char] = (charCounts[char] || 0) + 1;
+  }
+  for (const char of s) {
+    if (charCounts[char] === 1) {
+      return char;
+    }
+  }
+  return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
